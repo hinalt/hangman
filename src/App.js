a172fc102404c09fe3b2aa7accb6c7215f3a71df
kdhone24011996 <kdhone24011996@gmail.com>
2021-04-27 14:01:29 +0530

    j

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.scss';
 import bar from './assets/images/bar.png'
 import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
-import  folk1 from './assets/images/1.png'
+import  folk2 from './assets/images/1.png'
+import  folk3 from './assets/images/1.png'
+import  folk4 from './assets/images/1.png'
+import  folk5 from './assets/images/1.png'
+import  folk6 from './assets/images/1.png'
+import  folk7 from './assets/images/1.png'
+import  folk8 from './assets/images/1.png'
+import  folk9 from './assets/images/1.png'
+
+
 function App() {
   return (
    <div className="App">
@@ -18,6 +21,11 @@ function App() {
        <img src={folk1}/>
      </div>
      </div>
+   
+   <div>
+   <input type="text" placeholder="please enter a letter"/>
+   <button>submit</button>
+   </div>
    
    </div>
   );
