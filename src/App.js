a7f4dbcdc897a59d137473a31362bcf6e528dae9
kdhone24011996 <kdhone24011996@gmail.com>
2021-04-27 13:52:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.scss';
+import bar from './assets/images/bar.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
+import  folk1 from './assets/images/1.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <div className="bar">
+        <img src={bar} alt="bar"/>
+      <div  className="folk">
+       <img src={folk1}/>
+     </div>
+     </div>
+   
+   </div>
   );
 }
 
